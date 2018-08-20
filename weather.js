38d84c8651befa49e21b0bb51482fa5f8f1a3201
naganao <32755305+naganao@users.noreply.github.com>
2018-08-20 12:16:36 +0900

    initial commit
    initial commit

---
+++ weather.js
@@ -0,0 +1,17 @@
+$(function () {
+  var city = 'Wakayama';
+  var key = '44808d9a46863e33a7d8c9396dfeedbe';
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',JP&units=metric&APPID=' + key;
+  $.getJSON(url, function (data) {
+    if (!data.cod || data.cod != 200) {
+      return;
+    }
+
+    $("#temp").text(data.main.temp);
+    $("#temp_max").text(data.main.temp_max);
+    $("#temp_min").text(data.main.temp_min);
+    var icon = data.weather[0].icon;
+    $("#weather_icon").html('<img src="http://openweathermap.org/img/w/' + icon + '.png">')
+    $("#weather").text(data.weather[0].main);
+  });
+});
